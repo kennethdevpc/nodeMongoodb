319b2514acc9312761c5d245585a97e430c5be97
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-22 17:13:58 -0500

    boostrap and fontawesome configuration

--- src/server.js
+++ src/server.js
@@ -1,20 +1,31 @@
-const express = require ('express');
-const path = require ('path')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 //initializations
 const app = express();
 
 //Settings
-app.set('port',process.env.PORT || 4000)
-app.set('views', path.join( __dirname+'views'))
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname + '/views'));
+app.engine(
+  //configuracion del motor de plantillas
+  '.hbs',
+  exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs'); //aqui el motor de plantillas es engine
 //Middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 //Gloval variables
 
 //Routes
-app.get('/',(req,res)=>{
-    res.send("holamundo");
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
 //Static files
-app.use(express.static(path.join( __dirname+'public')))       //express.static(): define donde esta la carpeta pulica
-module.exports=app;
-
+app.use(express.static(path.join(__dirname + '/public'))); //express.static(): define donde esta la carpeta pulica
+module.exports = app;
