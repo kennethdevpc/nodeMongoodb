6113bfef965cfef412f3c93bebbad4ad4b0f0063
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-29 22:01:42 -0500

    registro de usuarios

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,10 +1,40 @@
 const usersCtrl = {};
+
+const User = require('../models/User');
 usersCtrl.renderSignUpForm = (req, res) => {
   res.render('users/signup');
 };
 
-usersCtrl.signup = (req, res) => {
-  res.send('signup');
+usersCtrl.signup = async (req, res) => {
+  const errors = [];
+  const { name, email, password, confirm_password } = req.body;
+  if (password != confirm_password) {
+    errors.push({ text: 'Password not match' });
+  }
+  if (password.length < 4) {
+    errors.push({ text: 'password must be at least 4 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('users/signup', {
+      errors,
+      name,
+      email,
+      password,
+      confirm_password,
+    });
+  } else {
+    const emailUser = await User.findOne({ email: email });
+    if (emailUser) {
+      req.flash('error_msg', 'The mail is already in use');
+      res.redirect('/users/signup');
+    } else {
+      const newUser = new User({ name, email, password });
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', 'You are registered ');
+      res.redirect('/users/signin');
+    }
+  }
 };
 
 //signin
