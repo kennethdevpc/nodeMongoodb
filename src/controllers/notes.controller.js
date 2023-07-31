b64a6beeaa2a8f8d8b62f5d2e3c9d4a5f7ec0cb4
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-30 21:01:22 -0500

    ahora puedo verificar si las notas le aparezcan segun el usuario

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -8,18 +8,23 @@ notesCtrl.renderNoteForm = (req, res) => {
 notesCtrl.createNewNote = async (req, res) => {
   const { title, description } = req.body;
   const newNote = new Note({ title, description });
+  newNote.user = req.user.id;
   await newNote.save();
   req.flash('success_msg', 'Note add successfuly');
   console.log(newNote);
   res.redirect('/notes');
 };
 notesCtrl.renderNotes = async (req, res) => {
-  const notes = await Note.find().lean();
+  const notes = await Note.find({ user: req.user.id }).sort({ createdAt: 'desc' }).lean();
   console.log(notes);
   res.render('notes/all-notes', { notes });
 };
 notesCtrl.renderEditForm = async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
+  if (note.user != req.user.id) {
+    req.flash('error_msg', 'Not authorized to edit this note');
+    res.redirect('/notes');
+  }
   console.log(note);
   res.render('notes/edit-note', { note });
 };
@@ -31,6 +36,11 @@ notesCtrl.updateNote = async (req, res) => {
   res.redirect('/notes');
 };
 notesCtrl.deleteNote = async (req, res) => {
+  const note = await Note.findById(req.params.id).lean();
+  if (note.user != req.user.id) {
+    req.flash('error_msg', 'Not authorized to edit this note');
+    res.redirect('/notes');
+  }
   await Note.findByIdAndDelete(req.params.id);
   req.flash('success_msg', 'Note Delete successfully');
 
