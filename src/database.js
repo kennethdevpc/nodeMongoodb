d04238dc303c9c75158ffbd1e4b8f3f490505e89
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-30 22:58:26 -0500

    database

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_URI_C } = process.env;
+const MONGODB_URI_l = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const MONGODB_URI = MONGODB_URI_C;
 mongoose
-  .connect(MONGODB_URI, {
+  .connect(MONGODB_URI || MONGODB_URI_l, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
